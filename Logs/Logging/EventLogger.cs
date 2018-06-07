57eb3c621dabd7263ee894fdda2f9bad0bd823b1
Jason MacLean <jmaclean@a-m-c.com>
2018-06-07 09:30:57 -0700

    Initial Commit

---
+++ Logs/Logging/EventLogger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+namespace AMCCommon
+{
+    class EventLogger
+    {
+        
+        private string Application;
+        private string EventLogName;
+
+        ///
+        /// Constructor
+        ///
+        /// The application doing the logging /// The log to write to in the Event Viewer 
+        public EventLogger(string app, string log)
+        {
+            Application = app;
+            EventLogName = log;
+
+            // Create the event log if it doesn't exist
+            if (!EventLog.SourceExists(Application))
+            {
+                EventLog.CreateEventSource(Application, EventLogName);
+            }
+        }
+
+        ///
+        /// Write to the event log
+        ///
+        /// The message to write         
+        public void WriteToEventLog(string message, string type)
+        {
+            switch (type.ToUpper())
+            {
+                case "INFO":
+                    EventLog.WriteEntry(Application, message, EventLogEntryType.Information);
+                    break;
+                case "ERROR":
+                    EventLog.WriteEntry(Application, message, EventLogEntryType.Error);
+                    break;
+                case "WARN":
+                    EventLog.WriteEntry(Application, message, EventLogEntryType.Warning);
+                    break;
+                default:
+                    EventLog.WriteEntry(Application, message, EventLogEntryType.Information);
+                    break;
+            }
+        }
+    }
+}
+
+
+
---
+++ Logs/Logging/Log.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace AMCCommon
+{
+    public class Log
+    {
+        public StreamWriter TheStream;
+        public string _sLogFileName = "";
+
+        public bool LogUserName { get; set; }
+
+        public Log(string sPath, bool bUserName = false, bool bDefaultName = false)
+        {
+            if (bDefaultName)
+                _sLogFileName = "Test_File.txt";
+            else
+                GetLogFileName(sPath);
+
+            TheStream = new StreamWriter(_sLogFileName, append: true);
+            LogUserName = bUserName;
+        }
+
+        private void GetLogFileName(string sPath)
+        {
+            // ja - create a valid log name with todays date and time in the current directory 
+            string sDir = sPath;
+
+            if (!Directory.Exists(sDir))
+            {
+                Directory.CreateDirectory(sDir);
+            }
+
+            if (LogUserName)
+                _sLogFileName = sDir + @"\" + GetUserName() + ":" + DateTime.Now.ToString("yyyyMMdd-HHmm") + @".log";
+            else
+                _sLogFileName = sDir + @"\" + DateTime.Now.ToString("yyyyMMdd-HHmm") + @".log";
+        }
+
+        public void WriteInfo(string sInfo)
+        {
+            TheStream.WriteLine(sInfo);
+            Console.WriteLine(sInfo);
+            TheStream.Flush();
+        }
+
+        public string GetUserName()
+        {
+            return Environment.UserName;
+        }
+
+        public void CloseLogging()
+        {
+            TheStream.Close();
+        }
+    }
+}
